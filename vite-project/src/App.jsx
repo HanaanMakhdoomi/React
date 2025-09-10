4714eb25b8293d773f513b86b44ad7d020db0dd1
HanaanMakhdoomi <hanaan.makhdoomi2023@vitstudent.ac.in>
2025-09-10 22:56:26 +0530

    Day 2

--- vite-project/src/App.jsx
+++ vite-project/src/App.jsx
@@ -1,10 +1,13 @@
 
-
+import Hanaan from './hanaan'
 function App() {
   
 
   return (
+    <>
+    <Hanaan/>
    <h1>Hi I am learning react with vite</h1>
+    </>
   )
 }
 
---
+++ vite-project/src/hanaan.jsx
@@ -0,0 +1,7 @@
+function Hanaan(){
+    return(
+        <h2> Hi i am hanaan</h2>
+    )
+
+}
+export default Hanaan
